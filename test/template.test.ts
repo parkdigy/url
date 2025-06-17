2ae61d86b054748b35ca6c1a60f6a97538a992b1
parkdigy <parkdigy@gmail.com>
2025-06-17 12:09:08 +0900

    save

--- test/template.test.ts
+++ test/template.test.ts
@@ -1,5 +1,5 @@
 describe('test template', () => {
-  it('should encode the data to base64', () => {
+  it('test', () => {
     const data = 'Hello, World!';
     const resultData = data.toLowerCase();
     expect(resultData).toBe('hello, world!');
